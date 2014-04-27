51df81469e433eb03eaa568b82a8fbb5db3c59a4
Alex Nichol <unixpickle@gmail.com>
2014-04-27 15:21:39 -0400

    Initial Commit

---
+++ FuncAnim3D/ANAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ANAppDelegate.h
+//  FuncAnim3D
+//
+//  Created by Alex Nichol on 4/27/14.
+//  Copyright (c) 2014 Alex Nichol. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ANAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ FuncAnim3D/ANAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  ANAppDelegate.m
+//  FuncAnim3D
+//
+//  Created by Alex Nichol on 4/27/14.
+//  Copyright (c) 2014 Alex Nichol. All rights reserved.
+//
+
+#import "ANAppDelegate.h"
+
+@implementation ANAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+  // Insert code here to initialize your application
+}
+
+@end
